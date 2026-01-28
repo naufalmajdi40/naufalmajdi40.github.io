9f65bcc1ab5dc181b6240c85c1c797c90d233d9f
Naufal Majdi <naufalmajdi40@gmail.com>
2026-01-28 10:04:13 +0700

    first upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const game = [
+    {
+        title: "Typing JS Game",
+        desc: "Test your typing skills and Javascript skill with this simple game",
+        icon: "assets/typingjs.png",
+        link: "typingjs/index.html"
+    },
+    {
+        title: "Flappy Bird",
+        desc: "Game Flappy bird Clone",
+        icon: "assets/flappybird.png",
+        link: "fastflappybird/index.html"
+    },
+    {
+        title: "Snake",
+        desc: "game snake clone",
+        icon: "assets/snake.png",
+        link: "snake/index.html"
+    },
+]
+window.onload = () => {
+    let gameList = document.getElementById("card-container");
+    game.forEach((item) => {
+        gameList.innerHTML += `
+            <div class="card-cartoon">
+                <img src="${item.icon}" class="card-img" alt="Typing Game JS">
+                <div class="card-body">
+                    <h4>${item.title}</h4>
+                    <p>${item.desc}</p>
+                    <a href="${item.link}" class="btn-cartoon">▶ PLAY</a>
+                </div>
+            </div>
+
+      `
+    })
+}
