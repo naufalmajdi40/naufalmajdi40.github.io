575d2a2d2101a354331cc563df3c4f1d7d8eae66
Naufal Majdi <naufalmajdi40@gmail.com>
2026-09-21 11:47:15 +0700

    sembunyikan ztl

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ const game = [
 
 ]
 const software = [
-    {
-        title: "ZTL Designer",
-        desc: "Sofware untuk Design Printer Zebra ",
-        icon: "assets/zplstudio.png",
-        link: "ztldesigner/ztldesigner.html"
-    },
+    // {
+    //     title: "ZTL Designer",
+    //     desc: "Sofware untuk Design Printer Zebra ",
+    //     icon: "assets/zplstudio.png",
+    //     link: "ztldesigner/ztldesigner.html"
+    // },
     {
         title: "GLB Viewer",
         desc: "Sofware untuk Melihat file 3D GLB",
